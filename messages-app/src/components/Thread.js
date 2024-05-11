bcec8bc5d8caee44c585f34939a1cfe53da62bf9
IvanGoranov02 <ivangoranoff@gmail.com>
2024-05-11 19:58:00 +0300

    version one

--- messages-app/src/components/Thread.js
+++ messages-app/src/components/Thread.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const formatDate = (dateString) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
-    return new Date(dateString).toLocaleDateString(undefined, options);
+    return new Date(dateString).toLocaleDateString('en-US', options);
 }
 
 function Thread({ messages }) {
