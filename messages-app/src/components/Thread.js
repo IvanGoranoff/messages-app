bac1625d11568527ba553d618368bd31a987129b
IvanGoranov02 <ivangoranoff@gmail.com>
2024-05-12 18:50:27 +0300

    fixed styles, small changes neederd

--- messages-app/src/components/Thread.js
+++ messages-app/src/components/Thread.js
@@ -10,10 +10,20 @@ function Thread({ messages }) {
     const toggleExpanded = () => setIsExpanded(!isExpanded);
     const highScoreExists = messages.some(msg => msg.score >= 6);
 
+    const renderPlaceholders = () => {
+        // Проверява дали екранът не е разгърнат и има повече от едно съобщение
+        return !isExpanded && messages.length > 1 && Array.from({ length: messages.length - 1 }, (_, index) => (
+            <div key={index} className="placeholder-card" style={{
+                zIndex: 2 - index, // Намалява z-index за всеки следващ плейсхолдер, започвайки от 2
+                transform: `translate(${10 * (index + 1)}px, ${10 * (index + 1)}px)` // Нарастващо изместване
+            }}></div>
+        ));
+    };
+
     return (
-        <div className="thread">
-            <div className={`message-card ${highScoreExists ? 'high-rating' : ''}`}
-                onClick={toggleExpanded}>
+        <div className="thread" onClick={toggleExpanded} aria-expanded={isExpanded}>
+            {renderPlaceholders()} {/* Render placeholders first */}
+            <div className={`message-card ${highScoreExists ? 'high-rating' : ''}`} style={{ zIndex: 5 }}>
                 <div className="metadata-content">
                     <h4 style={{ color: messages[0].score >= 6 ? '#9335ff' : '#e89d40' }}>{messages[0].subject}</h4>
                     <p>{messages[0].question}</p>
@@ -25,12 +35,12 @@ function Thread({ messages }) {
                 </div>
                 {messages.length > 1 && !isExpanded && (
                     <div className="count" style={{ backgroundColor: messages[0].score >= 6 ? '#27aae1' : '#e89d40' }}>
-                        {messages.length}  messages
+                        {messages.length} messages
                     </div>
                 )}
             </div>
             {isExpanded && messages.slice(1).map((message, index) => (
-                <div key={index} className="message-card">
+                <div key={index} className="message-card" style={{ marginTop: '10px' }}>
                     <div className="metadata-content">
                         <h4 style={{ color: message.score >= 6 ? '#9335ff' : '#e89d40' }}>{message.subject}</h4>
                         <p>{message.question}</p>
@@ -44,7 +54,7 @@ function Thread({ messages }) {
             ))}
         </div>
     );
-}
 
+}
 
 export default Thread;
