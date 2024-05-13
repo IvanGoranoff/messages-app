1b2a80b235bb610f80644b440fcca72943ca5b2d
IvanGoranov02 <ivangoranoff@gmail.com>
2024-05-13 10:49:56 +0300

    final fixed only date need litle fix

--- messages-app/src/components/Thread.js
+++ messages-app/src/components/Thread.js
@@ -11,18 +11,17 @@ function Thread({ messages }) {
     const highScoreExists = messages.some(msg => msg.score >= 6);
 
     const renderPlaceholders = () => {
-        // Проверява дали екранът не е разгърнат и има повече от едно съобщение
         return !isExpanded && messages.length > 1 && Array.from({ length: messages.length - 1 }, (_, index) => (
             <div key={index} className="placeholder-card" style={{
-                zIndex: 2 - index, // Намалява z-index за всеки следващ плейсхолдер, започвайки от 2
-                transform: `translate(${10 * (index + 1)}px, ${10 * (index + 1)}px)` // Нарастващо изместване
+                zIndex: 2 - index,
+                transform: `translate(${10 * (index + 1)}px, ${10 * (index + 1)}px)`
             }}></div>
         ));
     };
 
     return (
         <div className="thread" onClick={toggleExpanded} aria-expanded={isExpanded}>
-            {renderPlaceholders()} {/* Render placeholders first */}
+            {renderPlaceholders()}
             <div className={`message-card ${highScoreExists ? 'high-rating' : ''}`} style={{ zIndex: 5 }}>
                 <div className="metadata-content">
                     <h4 style={{ color: messages[0].score >= 6 ? '#9335ff' : '#e89d40' }}>{messages[0].subject}</h4>
@@ -30,7 +29,7 @@ function Thread({ messages }) {
                     <p style={{ fontSize: '18px' }}>{messages[0].text}</p>
                 </div>
                 <div className="metadata-details">
-                    <h5>{messages[0].team}</h5>
+                    <h5 id="team">{messages[0].team}</h5>
                     <h5>{formatDate(messages[0].created_at)}</h5>
                 </div>
                 {messages.length > 1 && !isExpanded && (
