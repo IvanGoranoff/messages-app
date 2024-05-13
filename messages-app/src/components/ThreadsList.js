6fa72a47e854aa6b90db220c120b09d97e51debb
IvanGoranov02 <ivangoranoff@gmail.com>
2024-05-13 08:33:14 +0300

    added json server

--- messages-app/src/components/ThreadsList.js
+++ messages-app/src/components/ThreadsList.js
@@ -1,15 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Thread from './Thread';
-import jsonData from '../services/data.json';
 
 function ThreadsList() {
-  const [threads, setThreads] = useState(jsonData.threads);
+  const [threads, setThreads] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/threads')
+      .then(response => {
+        if (!response.ok) {  // Handle non-2xx HTTP responses
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        setThreads(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error("Failed to fetch threads:", error);
+        setError('Failed to fetch data');
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div>
+      {loading && <div>Loading...</div>}
       {error && <div>Error: {error}</div>}
-      {threads.map((thread, index) => (
+      {!loading && !error && threads.map((thread, index) => (
         <Thread key={index} messages={thread} />
       ))}
     </div>
